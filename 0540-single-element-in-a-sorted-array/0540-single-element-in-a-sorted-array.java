57f1acef133d484e1016b8d45edd74a35afef84b
Bishal Chatterjee <86343196+bishalchatterjee@users.noreply.github.com>
2023-10-23 12:57:35 +0530

    Time: 0 ms (100%), Space: 49.6 MB (73.18%) - LeetHub

---
+++ 0540-single-element-in-a-sorted-array/0540-single-element-in-a-sorted-array.java
@@ -0,0 +1,35 @@
+class Solution {
+    // 0 1 2 3 4 5 6 7 8
+    //[1,1,2,3,3,4,4,8,8]
+    public int singleNonDuplicate(int[] nums) {
+        int n=nums.length;
+        
+        //trim down the search space by eleminating base case at first - to avoid edge cases in actual binary search space 
+
+        if(n==1) return nums[0];
+        //first element is the single element
+        if(nums[0]!=nums[1]) return nums[0];
+        //last element is the single element
+        if(nums[n-1]!=nums[n-2]) return nums[n-1];
+
+        //since first and last elements have been already checked and search space is reduced set low and high accordingly
+        int low=1;
+        int high=nums.length-2;
+
+        while(low<=high){
+            int mid=low+(high-low)/2;
+
+            //search 
+            if((nums[mid] != nums[mid-1]) && (nums[mid] != nums[mid+1])) return nums[mid];
+
+            //elemenate - (even,odd) - left half, sicne signle element is on the right half
+            if((mid%2==0 && nums[mid]==nums[mid+1]) || (mid%2!=0 && nums[mid]==nums[mid-1])){
+                low=mid+1;
+            }else{
+            //elemenate - (odd,even) - right half, since single elemenet is on the left half
+                high=mid-1;
+            }
+        }
+        return -1; //dummy statement & will never execute as there always exists a single element
+    }
+}
