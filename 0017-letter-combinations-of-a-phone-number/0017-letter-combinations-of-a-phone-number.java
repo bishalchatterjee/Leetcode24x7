77ff359cf4a2ed7801f6992024a53ac9db08c7cf
Bishal Chatterjee <86343196+bishalchatterjee@users.noreply.github.com>
2023-11-13 17:03:44 +0530

    Time: 5 ms (43.64%), Space: 41.5 MB (10.44%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.java
@@ -1,6 +1,46 @@
+/*
+//Approach 1: Use string digits as it is and apply recursion 
 class Solution {
     public List<String> letterCombinations(String digits) {
-        if (digits == null || digits.isEmpty()) {
+        if (digits.length()==0) {
+            return new ArrayList<>();
+        }
+
+        Map<Character, String> digitMap = new HashMap<>();
+        digitMap.put('2', "abc");
+        digitMap.put('3', "def");
+        digitMap.put('4', "ghi");
+        digitMap.put('5', "jkl");
+        digitMap.put('6', "mno");
+        digitMap.put('7', "pqrs");
+        digitMap.put('8', "tuv");
+        digitMap.put('9', "wxyz");
+
+        List<String> res = new ArrayList<>();
+        
+        recursiveHelper(digits, "", digitMap, res, 0);
+        return res;
+    }
+
+    private void recursiveHelper(String digits, String tempString, Map<Character, String> digitMap, List<String> res,int index) {
+        if(index==digits.length()){
+            res.add(new String(tempString));
+            return;
+        }
+
+        for(char c:digitMap.get(digits.charAt(index)).toCharArray()){
+            recursiveHelper(digits, tempString + c, digitMap, res, index+1);
+        }
+    }
+}
+*/
+
+
+//Approach 1: Convert digits to int and perform recursion Logic
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits == null || digits.length()==0) {
             return new ArrayList<>();
         }
 
@@ -19,11 +59,11 @@ class Solution {
         // Convert string digits to integer
         int equivalentNumber = Integer.parseInt(digits);
         
-        backtrackAndUpdate(equivalentNumber, "", digitMap, res);
+        recursiveHelper(equivalentNumber, "", digitMap, res);
         return res;
     }
 
-    private void backtrackAndUpdate(int num, String tempString, Map<Integer, String> digitMap, List<String> res) {
+    private void recursiveHelper(int num, String tempString, Map<Integer, String> digitMap, List<String> res) {
         if (num == 0) {
             res.add(new String(tempString));
             return;
@@ -33,7 +73,7 @@ class Solution {
         String keyLastDigit = digitMap.get(lastDigit);
 
         for (int i = 0; i < keyLastDigit.length(); i++) {
-            backtrackAndUpdate(num / 10, keyLastDigit.charAt(i) + tempString, digitMap, res);
+            recursiveHelper(num / 10, keyLastDigit.charAt(i) + tempString, digitMap, res);
         }
     }
 }
