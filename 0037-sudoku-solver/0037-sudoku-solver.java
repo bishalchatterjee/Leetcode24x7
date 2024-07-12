0c8e5cbfb36949d1222a606e9757b08542633e9e
Bishal Chatterjee <86343196+bishalchatterjee@users.noreply.github.com>
2024-07-12 21:21:10 +0530

    Time: 10 ms (47.03%), Space: 40.7 MB (71.96%) - LeetHub

---
+++ 0037-sudoku-solver/0037-sudoku-solver.java
@@ -0,0 +1,44 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        helperBacktrack(board);    
+    }
+
+    private boolean helperBacktrack(char[][] board) {
+        int row = board.length;
+        int col = board[0].length;
+
+        for(int i = 0; i < row; i++){
+            for(int j = 0; j < col; j++){
+                
+                if(board[i][j] == '.'){ //found the empty cell 
+                    
+                    for(char c = '1'; c <= '9'; c++){
+
+                        if(isValid(board, i, j, c)){
+                            
+                            board[i][j] = c;
+    
+                            if(helperBacktrack(board)) 
+                                return true;
+                            else
+                                board[i][j] = '.';  
+                        }
+                    }
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c){
+        for(int i = 0; i < 9; i++){
+            if(board[row][i] == c) return false;
+
+            if(board[i][col] == c) return false;
+
+            if(board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
