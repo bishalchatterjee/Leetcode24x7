9847b989fd294e238472caa2eda09b15a1917296
Bishal Chatterjee <86343196+bishalchatterjee@users.noreply.github.com>
2024-07-20 14:15:10 +0530

    Time: 6 ms (76.89%), Space: 52.8 MB (72.91%) - LeetHub

--- 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/1711-find-valid-matrix-given-row-and-column-sums.java
@@ -2,36 +2,6 @@ class Solution {
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
         int[][] resMatrix = new int[rowSum.length][colSum.length];
 
-        int currRow = 0;
-        int currCol = 0; 
-
-        while(currRow < rowSum.length && currCol < colSum.length){
-            // Assigning the minimum value curRowSum and currColSum
-            int value = Math.min(rowSum[currRow], colSum[currCol]);
-            resMatrix[currRow][currCol] = value;
-                
-            rowSum[currRow] -= value; //Updating the value
-            colSum[currCol] -= value;
-            
-            if(rowSum[currRow] == 0) 
-                currRow++;
-            else 
-                currCol++;
-        }
-        return resMatrix;
-    }
-}
-
-/*
-Approach : Brute 
-
-T.C - O(m * n)
-S.C - O(1)
-
-class Solution {
-    public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
-        int[][] resMatrix = new int[rowSum.length][colSum.length];
-
         for (int row = 0; row < rowSum.length; row++) {
             for (int col = 0; col < colSum.length; col++) {
                 // Assigning the minimum value curRowSum and currColSum
@@ -46,5 +16,3 @@ class Solution {
         return resMatrix;
     }
 }
-
-*/
