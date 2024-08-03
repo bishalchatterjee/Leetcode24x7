60b2b7811291a13c973947f4278c9627710496c1
Bishal Chatterjee <86343196+bishalchatterjee@users.noreply.github.com>
2024-08-03 11:47:48 +0530

    Time: 11 ms (8.75%), Space: 44.6 MB (18.11%) - LeetHub

---
+++ 1556-make-two-arrays-equal-by-reversing-subarrays/1556-make-two-arrays-equal-by-reversing-subarrays.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        Map<Integer, Integer> freqMapTarget = new HashMap<>();
+        Map<Integer, Integer> freqMapArr = new HashMap<>();
+
+        for (int x : target) {
+            freqMapTarget.put(x, freqMapTarget.getOrDefault(x, 0) + 1);
+        }
+
+        for (int x : arr) {
+            freqMapArr.put(x, freqMapArr.getOrDefault(x, 0) + 1);
+        }
+
+        for (Map.Entry<Integer, Integer> entry : freqMapArr.entrySet()) {
+            int key = entry.getKey();
+            int value = entry.getValue();
+            if (!freqMapTarget.containsKey(key) || !freqMapTarget.get(key).equals(value)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
