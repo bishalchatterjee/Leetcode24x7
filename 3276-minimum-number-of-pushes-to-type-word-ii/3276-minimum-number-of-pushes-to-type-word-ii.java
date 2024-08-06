cb3979e6f5b9328370a436e0685c676fe5664ab3
Bishal Chatterjee <86343196+bishalchatterjee@users.noreply.github.com>
2024-08-06 19:17:52 +0530

    Time: 44 ms (32.5%), Space: 45.7 MB (45.19%) - LeetHub

--- 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/3276-minimum-number-of-pushes-to-type-word-ii.java
@@ -11,10 +11,12 @@ class Solution {
         }
         Arrays.sort(sortedFreq, (x, y) -> y - x);
         
+        // x - 2 , y - 1, z - 1
+
         int totalPresses = 0;
         for (int i = 0; i < 26; i++) {
-
             if (sortedFreq[i] == 0) break;
+            System.out.println(i + " " + sortedFreq[i]);
             totalPresses += (i / 8 + 1) * sortedFreq[i];
         }
         
